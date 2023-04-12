27ccca595811abb5fd521ffd2531da8f26cbe74f
Denis <denis.were@student.moringaschool.com>
2023-04-12 17:26:40 +0300

    resubmission

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    let hq = 42; 
+    return Math.abs(someValue - hq);
+  }
+  function distanceFromHqInFeet(someValue) {
+    let blocks = distanceFromHqInBlocks(someValue);
+    return blocks * 264; 
+  }
+  function distanceTravelledInFeet(start, destination) {
+    let distance = Math.abs(start - destination) * 264; 
+    return distance;
+  }
+  function calculatesFarePrice(start, destination) {
+    let distance = distanceTravelledInFeet(start, destination);
+    let fare;
+  
+    if (distance <= 400) {
+      fare = 0;
+    } else if (distance <= 2000) {
+      fare = (distance - 400) * 0.02;
+    } else if (distance <= 2500) {
+      fare = 25;
+    } else {
+      return 'cannot travel that far';
+    }
+  
+    return fare;
+  }
+        
+
